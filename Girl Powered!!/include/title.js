a1609b9ddbd9a2f253513fdb7a93e44e19694aed
Kevin Grasso <kevgrasso@gmail.com>
2011-08-04 16:59:26 -0400

    corrected frame length

--- Girl Powered!!/include/title.js
+++ Girl Powered!!/include/title.js
@@ -79,7 +79,7 @@
 //
 //combos have minor effect on damage, major effect on pickups
 
-VIEWPORT.setFrameLength(1000);
+VIEWPORT.setFrameLength(50);
 INPUT.setKeys({27: 'esc', 49: '1', 50: '2', 51: '3', 39: 'right', 38: 'up', 37: 'left', 40: 'down', 65: 'a', 87: 'w', 83: 's', 68: 'd', 32: 'space'});
 
 (function () {
