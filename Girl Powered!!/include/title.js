81533f900c0e927f828626b6d1d6376789606cdc
Kevin Grasso <kevgrasso@gmail.com>
2011-09-30 20:23:28 -0400

    placeholder physics for testing

--- Girl Powered!!/include/title.js
+++ Girl Powered!!/include/title.js
@@ -80,6 +80,31 @@
 //combos have minor effect on damage, major effect on pickups
 
 VIEWPORT.setFrameLength(50);
+INPUT.setKeys({
+	w: 87,
+	s: 83,
+	a: 65,
+	d: 68,
+	up: 38,
+	down: 40,
+	left: 37,
+	right: 39,
+	z: 90,
+	x: 88,
+	c: 67,
+	v: 86,
+	space: 32,
+	esc: 27,
+	enter: 13,
+	backspace: 8,
+	tab: 9,
+	shift: 16,
+	ctrl: 17,
+	alt: 18,
+	1: 49,
+	2: 50,
+	3: 51
+});
 
 (function () {
 	var that = include.targets.title.pop(),
@@ -144,284 +169,155 @@ VIEWPORT.setFrameLength(50);
 			height: 150
 		} }
 	});
-	
-	player.id = 'jess';
-	player.xspeed = 0;
-	player.yspeed = 0;
-	player.jumpState = 'walk';
-	player.dashState = 'ready';
-	player.stats = {
-	//jess:
-	// speed med, but relatively faster run
-	// jump bit lower than megan, swing hook, umbrella (ride thermals)
-	// grav high
-	// max grav high
-	// range med
-	// recharge low
-	// attack low
-	// juice high
-	// whelm high
-	// wide phys and stun attack
-	// fire
-	//megan:
-	// speed high
-	// jump high, wall stick+jump, floaty
-	// grav low
-	// max grav low
-	// range low
-	// recharge high
-	// attack high
-	// juice low
-	// whelm low
-	// combo phys and no stun attack, air attack
-	// phys stronger than normal
-	// water
-	//kate:
-	// speed low
-	// jump low, double jump, jetpack, backflip
-	// grav high
-	// max grav mid
-	// range high
-	// recharge med
-	// attack med
-	// juice med
-	// whelm med
-	// no phys attack
-	// electricity
+	extend(player, {
+		id: 'jess',
+		xVel: 0,
+		yVel: 0,
+		jumpState: 'walk',
+		dashState: 'ready',
+		stats: {
+		//jess:
+		// speed med, but relatively faster run
+		// jump bit lower than megan, swing hook, umbrella (ride thermals)
+		// grav high
+		// max grav high
+		// range med
+		// recharge low
+		// attack low
+		// juice high
+		// whelm high
+		// wide phys and stun attack
+		// fire
+		//megan:
+		// speed high
+		// jump high, wall stick+jump, floaty
+		// grav low
+		// max grav low
+		// range low
+		// recharge high
+		// attack high
+		// juice low
+		// whelm low
+		// combo phys and no stun attack, air attack
+		// phys stronger than normal
+		// water
+		//kate:
+		// speed low
+		// jump low, double jump, jetpack, backflip
+		// grav high
+		// max grav mid
+		// range high
+		// recharge med
+		// attack med
+		// juice med
+		// whelm med
+		// no phys attack
+		// electricity
+			
+			jess: {
+				fightVel: 4.1,
+				flightVel: 8.75,
+				skidVel: null,
+				skidTime: null,
+				
+				jumpVel: null,
+				ftJumpVel: null,
+				flJumpVel: null,
+				gravity: null,
+			}
 		
-		neostyle: {
-			flightVel: null,
-			fightVel: null,
-			dashVel: null,
-			dashWarp: null,
-			slowVel: null,
-			slowTime: null,
-			
-			jumpType: null,
-			jumpVel: null,
-			gravAcc: null,
-			jumpDeacc: null,
-			
-			rechargeIdleRate: null,
-			rechangeFlightRate: null,
-			rechargeFightRate: null,
-			rechargeAttackRate:null,
-			rechargeDelay: null,
-			
-			physType: null,
-			range: null,
-			element: null
-			//todo: unique hitboxes?
+//			jess: {
+//				ftVel: 4.1,
+//				flAcc: 4/14,
+//				
+//				deacc: 10/21,
+//				turnAcc: 4/14,
+//				maxVel: 8.75,
+//				
+//				jmpVel: -14,
+//				airAcc: 9/14,
+//				maxAirVel: 5
+//			}, 
+//			kate: {
+//				ftVel: 3.2,
+//				flAcc: 6/14,
+//				
+//				deacc: 16/21,
+//				turnAcc: 5/14,
+//				maxVel: 8,
+//				
+//				jmpVel: -13,
+//				airAcc: 7/14,
+//				maxAirVel: 4.5
+//			},
+//			megan: {
+//				ftVel: 4.8,
+//				flAcc: 5/14,
+//				
+//				deacc:8/21,
+//				turnAcc: 3/14,
+//				maxVel: 9.5,
+//				
+//				jmpVel: -15,
+//				airAcc: 5/14,
+//				maxAirVel: 4
+//			}
 		},
+		
+		step: TRIGGER.subscribe({
+			trigger: 'step',
+			context: 'global',
+			obj: player,
+			priority: 50,
+			func: function () {
+				that.collide = false;
+				
+				this.moveQueue.pushRel(this.xVel, null);
+				
+				return this.move();
+			}
+		}),
+		ftLeft: INPUT.request({
+			input: 'leftHold',
+			context: 'global',
+			obj: player,
+			priority: 50,
+			func: function () {
+				this.xVel -= this.stats[this.id].fightVel;
+			}
+		}),
+		ftLeft: INPUT.request({
+			input: 'leftUp',
+			context: 'global',
+			obj: player,
+			priority: 50,
+			func: function () {
+				this.xVel += this.stats[this.id].fightVel;
+			}
+		}),
+		ftRight: INPUT.request({
+			input: 'rightHold',
+			context: 'global',
+			obj: player,
+			priority: 50,
+			func: function () {
+				this.xVel += this.stats[this.id].fightVel;
+			}
+		}),
+		ftRightStop: INPUT.request({
+			input: 'rightUp',
+			context: 'global',
+			obj: player, 
+			priority: 50,
+			func: function () {
+				this.xVel -= this.stats[this.id].fightVel;
+			}
+		}),
+			
+	});
 	
-		jess: {
-			ftVel: 4.1,
-			flAcc: 4/14,
-			
-			deacc: 10/21,
-			turnAcc: 4/14,
-			maxVel: 8.75,
-			
-			jmpVel: -14,
-			airAcc: 9/14,
-			maxAirVel: 5
-		}, 
-		kate: {
-			ftVel: 3.2,
-			flAcc: 6/14,
-			
-			deacc: 16/21,
-			turnAcc: 5/14,
-			maxVel: 8,
-			
-			jmpVel: -13,
-			airAcc: 7/14,
-			maxAirVel: 4.5
-		},
-		megan: {
-			ftVel: 4.8,
-			flAcc: 5/14,
-			
-			deacc:8/21,
-			turnAcc: 3/14,
-			maxVel: 9.5,
-			
-			jmpVel: -15,
-			airAcc: 5/14,
-			maxAirVel: 4
-		}
-	};
 	
-	player.step = function () {
-		var stats,
-			xVel,
-			flightKey = 'neutral',
-			fightKey = 'neutral',
-			airKey = 'neutral',
-			jumpStart = false,
-			jumpKey = false,
-			dashKey = false,
-		
-			dirKey = 'neutral',
-			dirSign;
-		
-		
-		//PHYSICS
-		//warning: this info is obsolete
-		//24 frames to max  (1.20 seconds)
-		//17 frames to stop (0.85 seconds)
-		//08 frames to turn (0.40 seconds)
-		//
-		//xx.x pixels to max?
-		//xx.x pixels to stop?
-		//xx.x pixels to turn?
-		
-		that.collide = false;
-		
-		if (INPUT.state[1]) {
-			this.id = 'jess';
-		} else if (INPUT.state[2]) {
-			this.id = 'kate';
-		} else if (INPUT.state[3]) {
-			this.id = 'megan';
-		}
-		stats = this.stats[this.id];
-		
-		if (INPUT.state.up || INPUT.state.w) {
-			if ((INPUT.keydown.up <= 3 || INPUT.keydown.w <= 3) && !this.airborne) {
-				jumpStart = true;
-			}
-			jumpKey = true;
-		}
-		if (!INPUT.state.left || !INPUT.state.right) { //if both fight keys held, they cancel out
-			if (INPUT.state.right) {
-				fightKey = 'right';
-				if (INPUT.keydown.right === 0 && INPUT.keyup.right <= 3) {
-					dashKey = true;
-				}
-			} else if (INPUT.state.left) {
-				fightKey = 'left';
-				if (INPUT.keydown.left === 0 && INPUT.keyup.left <= 3) {
-					dashKey = true;
-				}
-			}
-		}
-		if (!INPUT.state.a || !INPUT.state.d) { //if both flight keys held, they cancel out
-			if (INPUT.state.d) {
-				flightKey = 'right';
-			} else if (INPUT.state.a) {
-				flightKey = 'left';
-			}
-		}
-		
-		if (jumpStart) {
-			this.airborne = true;
-			this.yspeed = stats.jmpVel-Math.abs(this.xspeed/7);
-		}
-		
-		if (this.airborne) {
-			if (this.yspeed < 0 && !jumpKey) {
-				this.yspeed *= 0.80;
-			}
-			this.yspeed += (Math.E/3); //gravity
-			this.moveQueue.pushRel(null, this.yspeed);
-			
-			if (this.y+this.yspeed >= 330) {
-				this.airborne = false;
-				this.yspeed = 0;
-				this.moveQueue.pushAbs(null, 330);
-			}
-		}
 	
-		if (this.xspeed === 0 && flightKey === 'neutral') {
-			if (fightKey !== 'neutral') {
-				this.facing = fightKey;
-			}
-		
-			if (fightKey === 'left') {
-				xVel = -stats.ftVel;
-			} else if (fightKey === 'right') {
-				xVel = stats.ftVel;
-			}
-		} else {
-			if (flightKey !== 'neutral') {
-				this.facing = flightKey;
-			}
-			
-			if (this.airborne) {
-				if (flightKey === 'left' && this.xspeed > -stats.maxAirVel) {
-					this.xspeed -= stats.airAcc;
-					if (this.xspeed < -stats.maxAirVel) {
-						this.xspeed = -stats.maxAirVel;
-					}
-				} else if (flightKey === 'right' && this.xspeed < stats.maxAirVel) {
-					this.xspeed += stats.airAcc;
-					if (this.xspeed > stats.maxAirVel) {
-						this.xspeed = stats.maxAirVel;
-					}
-				}
-				
-				xVel = this.xspeed;
-			} else {
-				
-				
-				if (this.xspeed < 0 || (this.xspeed === 0 && flightKey === 'left')) {
-					dirSign = -1;
-					if (flightKey === 'left') {
-						dirKey = 'forward';
-					} else if (flightKey === 'right') {
-						dirKey = 'reverse';
-					}
-				} else if (this.xspeed > 0 || (this.xspeed === 0 && flightKey === 'right')) {
-					dirSign = 1;
-					if (flightKey === 'right') {
-						dirKey = 'forward';
-					} else if (flightKey === 'left') {
-						dirKey = 'reverse';
-					}
-				}
-				
-				// running
-				if (dirKey === 'forward') {
-						//only cap speed by acceleration at 9.
-						//if they got above 9 some other way, don't touch it
-					if (Math.abs(this.xspeed) < stats.maxVel) {
-						this.xspeed += stats.flAcc *dirSign;
-						
-						if (Math.abs(this.xspeed) > stats.maxVel) {
-							this.xspeed = stats.maxVel *dirSign;
-						}
-					}
-					
-				// friction/deacceleration
-				} else if (dirKey !== 'forward') {
-					this.xspeed -= stats.deacc *dirSign;
-					if (dirKey === 'reverse') {
-						this.xspeed -= stats.turnAcc *dirSign;
-						
-						if (Math.abs(this.xspeed) <= stats.deacc + stats.turnAcc ) {
-							this.xspeed = 0;
-						}
-					} else if (Math.abs(this.xspeed) <= stats.deacc) {
-						this.xspeed = 0;
-					}
-				}
-				
-				xVel = this.xspeed;
-			}
-			
-		}
-		this.moveQueue.pushRel(xVel, null);
-		
-		return this.move();
-		
-	};
-	TRIGGER.subscribe({
-		trigger: 'step',
-		func: player.step,  
-		obj: player, 
-		priority: 50,
-		context: 'global'});
 	
 	test = new Actor({
 		x: 150,
